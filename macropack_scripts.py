d74514a368befba60e4d8397a310d578fa192f2b
anand <anand_talreja1@hotmail.com>
2021-11-29 19:30:46 +0500

    Folder managed

--- macropack_scripts.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-cwd = os.getcwd()
-a = subprocess.run([cwd+'\\executable_scripts\\macro_pack.exe','--listtemplates'])
-print(a)
-
-
---
+++ python_backend/macropack_scripts.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+cwd = os.getcwd()
+
+
+
+# a = subprocess.run([cwd+'\\executable_scripts\\macro_pack.exe','--listtemplates'])
+# print(a)
+
+
+def run_option(num):
+    switch={
+        1:'--listtemplates',
+        2:'-t EMBED_EXE --embed=c:\windows\system32\calc.exe -o -G my_calc.doc'
+
+    }
+
+    a = subprocess.run([cwd + '\\executable_scripts\\macro_pack.exe','-t EMBED_EXE --embed=c:\\windows\\system32\\calc.exe -o -G my_calc.doc'],shell=True)
+    print(a)
+
+run_option(2)
